b21a822223c9428ab9496b1ded667c34ebc18a14
a-kazoki <ahmedammarsalah@gmail.com>
2017-01-30 11:47:59 -0600

    adjust 6

--- js/main.js
+++ js/main.js
@@ -9,10 +9,17 @@ $(function () {
 });
  /* jQuery Preloader
   -----------------------------------------------*/
-$(window).ready(function () {
+/*$(window).load(function () {
     "use strict";
     $('.preloader').fadeOut(2000); // set duration in brackets    
+});*/
+$(window).on('load', function () {
+    "use strict";
+    $(".preloader").fadeOut(2000, function () {
+        $(this).remove();
+    });
 });
+
 // Toggle Script
 (function () {
     "use strict";
